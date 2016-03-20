5b9a50ce2c322e0c224b2022c2bea2c21f003675
Zoryn Aaron <zoryn4163@gmail.com>
2016-03-20 15:27:08 -0400

    init

---
+++ SMAPITestMod/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SMAPITestMod
+{
+    public class Class1
+    {
+    }
+}
