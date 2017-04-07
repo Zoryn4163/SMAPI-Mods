4986e3cf4a4f3d02d5823d1a006b3eea6728e466
Jesse Plamondon-Willard <github@jplamondonw.com>
2017-04-07 02:27:13 -0400

    remove RNG settings that aren't implemented

--- BetterRNG/RngConfig.cs
+++ BetterRNG/RngConfig.cs
@@ -11,10 +11,6 @@ namespace BetterRNG
         public int StormyChance { get; set; }
         public int HarshSnowyChance { get; set; }
 
-        public bool EnableFishingTreasureOverride { get; set; }
-        public float FishingTreasureChance { get; set; }
-        public bool EnableFishingStuffOverride { get; set; }
-
         /// <summary>Construct an instance.</summary>
         public RngConfig()
         {
@@ -22,8 +18,7 @@ namespace BetterRNG
             {
                 new [] {
                     "The weather chances are whole numbers as percentages.",
-                    "They can add up to be any number. 60 = 60% or 0.60, but you must type 60",
-                    "The fishing things are not done yet and literally do nothing. Do not bother changing their values."
+                    "They can add up to be any number. 60 = 60% or 0.60, but you must type 60"
                 }
             };
             this.SunnyChance = 60;
@@ -31,10 +26,6 @@ namespace BetterRNG
             this.RainyChance = 15;
             this.StormyChance = 5;
             this.HarshSnowyChance = 5;
-
-            this.EnableFishingTreasureOverride = false;
-            this.FishingTreasureChance = 1 / 16f;
-            this.EnableFishingStuffOverride = false;
         }
     }
 }
