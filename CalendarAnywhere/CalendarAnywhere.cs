f76a4da89c2c53e4ee788e912f0362f7e89d4c54
Jesse Plamondon-Willard <github@jplamondonw.com>
2017-04-07 13:15:14 -0400

    remove unused code

--- CalendarAnywhere/CalendarAnywhere.cs
+++ CalendarAnywhere/CalendarAnywhere.cs
@@ -18,9 +18,6 @@ namespace CalendarAnywhere
         *********/
         private MouseState MState;
 
-        private string OpenPath;
-        private Texture2D OpenTexture;
-
         private readonly Rectangle Target = new Rectangle(x: (Game1.graphics.GraphicsDevice.Viewport.Width - 300) + 108, y: (Game1.tileSize / 8) + 20, width: 160, height: 41);
 
         private Rectangle MouseRect => new Rectangle(Game1.oldMouseState.X, Game1.oldMouseState.Y, 64, 64);
@@ -34,20 +31,8 @@ namespace CalendarAnywhere
         /// <param name="helper">Provides simplified APIs for writing mods.</param>
         public override void Entry(IModHelper helper)
         {
-            this.OpenPath = Path.Combine(helper.DirectoryPath, "open.png");
-
             ControlEvents.MouseChanged += this.ControlEvents_MouseChanged;
             ControlEvents.ControllerButtonPressed += this.ControlEvents_ControllerButtonPressed;
-            GraphicsEvents.OnPostRenderEvent += this.GraphicsEvents_OnPostRenderEvent;
-
-            GameEvents.FirstUpdateTick += (sender, args) =>
-            {
-                if (File.Exists(OpenPath))
-                {
-                    using (FileStream fs = File.OpenRead(OpenPath))
-                        OpenTexture = Texture2D.FromStream(Game1.graphics.GraphicsDevice, fs);
-                }
-            };
         }
 
 
@@ -77,19 +62,6 @@ namespace CalendarAnywhere
 
             if (e.ButtonPressed == Buttons.A && this.Target.Contains(this.ClickPoint))
                 Game1.activeClickableMenu = new Billboard();
-        }
-
-        private void GraphicsEvents_OnPostRenderEvent(object sender, EventArgs e)
-        {
-            if (!Game1.hasLoadedGame || Game1.activeClickableMenu != null || OpenTexture == null)
-                return;
-
-            if (this.Target.Contains(Game1.getOldMouseX(), Game1.getOldMouseX()))
-            {
-                Game1.spriteBatch.Draw(OpenTexture, this.Target, Color.White);
-                Game1.spriteBatch.DrawString(Game1.smallFont, "Calendar", new Vector2(this.Target.X, this.Target.Y), Color.Black, 0, new Vector2(-3, -5), 1.4f, SpriteEffects.None, 0.001f);
-                Game1.spriteBatch.Draw(Game1.mouseCursors, MouseRect, new Rectangle(0, 0, 16, 16), Color.White);
-            }
         }
     }
 }
