d72fc07bc889458d1b36403bc59df59a35728cc2
Jesse Plamondon-Willard <github@jplamondonw.com>
2017-04-07 13:06:43 -0400

    update all versions for release

--- JunimoDepositAnywhere/Properties/AssemblyInfo.cs
+++ JunimoDepositAnywhere/Properties/AssemblyInfo.cs
@@ -3,5 +3,5 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("JunimoDepositAnywhere")]
 [assembly: Guid("e576d252-25ba-491a-a1be-4d24e47ba095")]
-[assembly: AssemblyVersion("1.5.0.0")]
-[assembly: AssemblyFileVersion("1.5.0.0")]
+[assembly: AssemblyVersion("1.6.0.0")]
+[assembly: AssemblyFileVersion("1.6.0.0")]
--- RegenMod/Properties/AssemblyInfo.cs
+++ RegenMod/Properties/AssemblyInfo.cs
@@ -3,5 +3,5 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("RegenMod")]
 [assembly: Guid("f0a0f913-a1ab-49ac-a1d9-eefcd7a5e0bc")]
-[assembly: AssemblyVersion("1.5.0.0")]
-[assembly: AssemblyFileVersion("1.5.0.0")]
+[assembly: AssemblyVersion("1.6.0.0")]
+[assembly: AssemblyFileVersion("1.6.0.0")]
