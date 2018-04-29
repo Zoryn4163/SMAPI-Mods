222fd0aa84936220349fb4c455403c73b22b3a2e
Jesse Plamondon-Willard <github@jplamondonw.com>
2018-04-29 01:18:33 -0400

    update versions for release

--- CalendarAnywhere/Properties/AssemblyInfo.cs
+++ CalendarAnywhere/Properties/AssemblyInfo.cs
@@ -3,5 +3,5 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("CalendarAnywhere")]
 [assembly: Guid("02a9ff32-12f5-44f0-9f0a-977ba8b56dc4")]
-[assembly: AssemblyVersion("1.8.0.0")]
-[assembly: AssemblyFileVersion("1.8.0.0")]
+[assembly: AssemblyVersion("1.9.0.0")]
+[assembly: AssemblyFileVersion("1.9.0.0")]
